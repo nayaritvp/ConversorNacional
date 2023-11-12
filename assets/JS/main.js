c30c478d3c4f290fe31de86227ab8b2250b118c8
Nayarit Vazquez <nayaritvp@gmail.com>
2023-11-12 20:39:35 -0300

    conversor de monedas

---
+++ assets/JS/main.js
@@ -0,0 +1,84 @@
+// Primero, define la función fetchApiPromise
+async function fetchApiPromise(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Error en la solicitud a la API. Código de respuesta: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data; 
+}
+
+// Luego, el resto de tu código
+async function main() {
+    try {
+        const data = await fetchApiPromise("https://mindicador.cl/api/");
+        const currencies = data.moneda;
+
+        const cantidadUno = document.getElementById("cantidad_uno");
+        const selectorDos = document.getElementById("selector_dos");
+        const cantidadDos = document.getElementById("cantidad_dos");
+        const botonBuscar = document.getElementById("botonbuscar");
+        const rateDiv = document.getElementById("rate");
+
+        botonBuscar.addEventListener("click", async function() {
+            try {
+                const data = await fetchApiPromise("https://mindicador.cl/api/");
+        
+                // Verifica que la respuesta tenga la estructura esperada
+                if (!data || !data.uf || !data.ivp || !data.dolar || !data.euro) {
+                    throw new Error("La respuesta de la API no tiene la estructura esperada.");
+                }
+        
+                const currencies = {
+                    "EUR": data.euro.valor,
+                    "USD": data.dolar.valor,
+                    // Agrega más monedas según sea necesario
+                };
+        
+                const cantidadUnoValue = parseFloat(cantidadUno.value);
+                const monedaOrigen = "CLP";
+                const monedaDestinoSeleccionada = selectorDos.value;
+        
+                if (isNaN(cantidadUnoValue)) {
+                    rateDiv.textContent = "Ingresa una cantidad válida.";
+                    return;
+                }
+        
+                if (monedaDestinoSeleccionada === "cual") {
+                    rateDiv.textContent = "Selecciona una moneda de destino.";
+                    return;
+                }
+        
+                // Utiliza directamente el valor de la moneda seleccionada
+                const tasaCambio = currencies[monedaDestinoSeleccionada];
+                const resultado = cantidadUnoValue / tasaCambio;
+        
+                cantidadDos.value = resultado.toFixed(2);
+                rateDiv.textContent = `1 ${monedaOrigen} = ${1 / tasaCambio} ${monedaDestinoSeleccionada}`;
+            } catch (error) {
+                console.error("Error al obtener la API:", error);
+                rateDiv.textContent = "Error al obtener los datos de la API.";
+            }
+        });
+
+        console.log(data);
+    } catch (error) {
+        console.error("Error al obtener la API:", error);
+    }
+}
+
+function mapMonedaSeleccionada(valorSeleccionado) {
+    switch (valorSeleccionado) {
+        case "Dólar":
+            return "dolar";
+        case "Euro":
+            return "euro";
+        // Agrega más casos según sea necesario
+        default:
+            return "";
+    }
+}
+
+// Finalmente, llama a la función main
+main();
